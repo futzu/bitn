88d5bb7b8a3c2f23cac5a732df9517f10e73a9fb
CupCake of Doom <52701496+futzu@users.noreply.github.com>
2019-12-31 03:43:12 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Super Fast Bit Slicer",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/futzu/bitslicer9k"
+    url="https://github.com/futzu/bitslicer9k",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
