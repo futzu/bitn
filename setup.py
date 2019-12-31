878d06fd98e436fec3b95430d70e18f93437eb52
futzu <52701496+futzu@users.noreply.github.com>
2019-12-31 14:10:15 -0500

    version bump to 1.0.13

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bitslicer9k",
-    version="1.0.11",
+    version="1.0.13",
     author="fu-corp",
     author_email="spam@futzu.com",
     description="Super Fast Bit Slicer",
