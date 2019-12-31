38327810e57030d8fcf3f0ccda3edea126e6afc9
CupCake of Doom <52701496+futzu@users.noreply.github.com>
2019-12-31 03:40:13 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bitslicer9k"
+    version="1.0.01,
+    author="fu-corp",
+    author_email="spam@futzu.com",
+    description="Super Fast Bit Slicer"
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/futzu/bitslicer9k"
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.1
+)
