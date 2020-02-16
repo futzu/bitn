2e2f0d19ac06c50310e8fd1373ac2e444f8f71ba
futzu <52701496+futzu@users.noreply.github.com>
2020-02-16 03:05:44 -0500

    version bumped to 0.0.15

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bitn",
-    version="0.0.13",
+    version="0.0.15",
     author="fu-corp",
     author_email="spam@futzu.com",
     description="Fast Bitwise for Mpegts Parsing.",
