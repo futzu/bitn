599a5ef8882f5b3e4980986e652310efb3d03732
Adrian <52701496+futzu@users.noreply.github.com>
2020-03-03 04:07:41 -0500

    profiled, now 33% faster.

--- bitn.py
+++ bitn.py
@@ -1,30 +1,28 @@
 class BitBin:
     def __init__(self,bites):
         '''
-        From bytes to bits
+        From bytes to bits.
         '''
-        if not isinstance(bites,bytes):
-            raise TypeError('bites needs to be type bytes')
-        self.bitsize = self.idx = (len(bites)*8)
+        lb=len(bites) << 3
+        self.bitsize =  lb
+        self.idx = lb
         self.bits=int.from_bytes(bites,byteorder='big')
 
     def as90k(self,num_bits):
         '''
         Returns num_bits 
         of bits as 90k time
-        ''' 
-        return f'{ (self.asint(num_bits)/90000):.6f}'
+        '''
+        t = (self.asint(num_bits)/90000.00)
+        return f'{t:.06f}'
           
     def asint(self,num_bits):
         '''
         Starting at self.idx of self.bits, 
         slice off num_bits of bits.
         ''' 
-        if num_bits > self.idx:
-            raise ValueError(f'reading {num_bits - self.idx} bits too many')
-        else:
-            self.idx =  self.idx-num_bits
-            return (self.bits >> (self.idx)) & ~(~0 << num_bits)
+        self.idx =  self.idx-num_bits
+        return (self.bits >> (self.idx)) & ~(~0 << num_bits)
              
     def ashex(self,num_bits):
         '''
@@ -38,14 +36,14 @@ class BitBin:
         Returns num_bits of bits 
         from hex decoded to bytes 
         '''
-        k= self.asint(num_bits)
+        k = self.asint(num_bits)
         return bytearray.fromhex(hex(k)[2:]).decode()   
                 
     def asflag(self,num_bits=1):
         '''
         Returns one bit as True or False
         '''
-        return  self.asint(num_bits) == 1
+        return  self.asint(num_bits) is 1
 
     def forward(self,num_bits):
         '''
@@ -53,8 +51,6 @@ class BitBin:
         forward by num_bits
         '''
         self.idx -= num_bits
-        if self.idx < 0:
-            self.idx = 0
                 
     def rewind(self,num_bits):
         '''
